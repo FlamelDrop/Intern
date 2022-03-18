a2b91701ddfb220ace58d5d899e5d28a97f0dc4f
Chanismon Thangprakob <flameldrop@gmail.com>
2022-03-19 02:20:13 +0700

    update

--- ChomCHOB/product-lists/src/App.js
+++ ChomCHOB/product-lists/src/App.js
@@ -4,20 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <div className="Header">
+        <div className="rectangle">
+          
+        </div>
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+        Learn React
+        </a> */}
+
       </header>
+
     </div>
   );
 }
