684b68ded5a1d2e3b7907829138b5874aa7edb8c
Chanismon Thangprakob <flameldrop@gmail.com>
2022-03-20 14:21:57 +0700

    onclick logo

--- ChomCHOB/product-lists/src/App.js
+++ ChomCHOB/product-lists/src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+var click1=document.querySelectorAll("d");
+var preclick1;
+for (var i=0;i<click1.length;i++){
+  click1[i].onclick=function(){
+    var classes=this.classList;
+    if(classes.contains("show")){
+      classes.remove("show")
+    }
+    else{
+      if(preclick1!=null)
+      preclick1.classList.remove("show");
+      preclick1=this;
+      classes.add("show");
+      // this.classList.toggle("show");
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
 
       <div className="Header">
         <div className="rectangle1">
-          <img src={logo} className="Logo"alt="logo"/>
+          <div className="Logo">
+            <img src={logo} alt="logo"/>
+            <d>LOGO!</d>
+          </div>
+
           <div className="Menu">
             <b1>Home</b1>
             <b2>New Products</b2>
